ae7c5f5e97941bf5a249e379293af2218a691339
s3722123 <s3722123@student.rmit.edu.au>
2019-08-25 17:05:25 +1000

    Initial Commit

---
+++ LK_Tab_Table_Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LK_Tab_Table_Collection
+//
+//  Created by Lachlan  Wilson on 25/8/19.
+//  Copyright © 2019 Lachlan  Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LK_Tab_Table_CollectionTests/LK_Tab_Table_CollectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  LK_Tab_Table_CollectionTests.swift
+//  LK_Tab_Table_CollectionTests
+//
+//  Created by Lachlan  Wilson on 25/8/19.
+//  Copyright © 2019 Lachlan  Wilson. All rights reserved.
+//
+
+import XCTest
+@testable import LK_Tab_Table_Collection
+
+class LK_Tab_Table_CollectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
